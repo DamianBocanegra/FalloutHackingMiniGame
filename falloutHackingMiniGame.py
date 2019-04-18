3142274eb22a927c7738f5471d58c754186b51cb
Damian <d_b172@txstate.edu>
2019-04-18 13:32:28 -0500

    Update falloutHackingMiniGame.py
    Basic Functions started writing Main

--- falloutHackingMiniGame.py
+++ falloutHackingMiniGame.py
@@ -1,11 +1,31 @@
 #Fallout Password Crack
 
-def addpassword(passwords, password):
-    passwords.append(password)
+def addpassword(list, password):
+    list.append(password)
 
-def checkAnswer(passwords, key):
-    for password in passwords:
-        for letter in password:
-            
+def checkAnswer(correctPassword, guess):
+    for letter in correctPassword:
+        correctLetters = 0
+        index = 0
+        if guess[index] != letter:
+            index += 1
+        else:
+            index += 1
+            correctLetters += 1
+        return correctLetters
+def printPasswordList(list):
+    for word in list:
+        print(word)
 
-passwords = {};
+
+
+def main():
+    #Fill in List of Passwords
+    passwords = []
+    entering = True
+    while entering:
+        #read passwords from file
+        addpassword(passwords, word)
+        
+    
+
