a3d4ef5f1e51c843ae2934057ba9f4bb860e43a3
Damian <d_b172@txstate.edu>
2019-04-18 13:21:09 -0500

    Create falloutHackingMiniGame.py
    Inital Commit

---
+++ falloutHackingMiniGame.py
@@ -0,0 +1,11 @@
+#Fallout Password Crack
+
+def addpassword(passwords, password):
+    passwords.append(password)
+
+def checkAnswer(passwords, key):
+    for password in passwords:
+        for letter in password:
+            
+
+passwords = {};
