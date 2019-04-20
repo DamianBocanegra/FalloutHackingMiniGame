b0e618b88a31cb5faf8320e21aa9e9150f88fdf3
Damian <d_b172@txstate.edu>
2019-04-20 16:22:46 -0500

    Update falloutHackingMiniGame.py
    Creating skeleton of function

--- falloutHackingMiniGame.py
+++ falloutHackingMiniGame.py
@@ -1,6 +1,7 @@
 #Fallout Password Crack
-
-def addpassword(list, password):
+#Recreate Fallout password cracking mini-game
+import random
+def addPassword(list, password):
     list.append(password)
 
 def checkAnswer(correctPassword, guess):
@@ -13,19 +14,42 @@ def checkAnswer(correctPassword, guess):
             index += 1
             correctLetters += 1
         return correctLetters
+
 def printPasswordList(list):
     for word in list:
         print(word)
 
+def enterPasswords(passwords):
+    entering = True
+    while entering:
+        word = raw_input("Please enter password: ")
+        if word != "0":
+            addPassword(passwords, word)
+        else:
+            entering = False
+    return passwords
 
+def guessPasswordsGame(passwords, correctKey):
+    playing = True
+    while playing:
+        printPasswordList(passwords)
+        playerInput = raw_input("Select Password: ")
+        correctLetters = checkAnswer(correctKey, playerInput)
+        if correctLetters < len(correctKey):
+            print("Incorrect. Letters: " + corrrectLetters + "/" + len(correctKey))
+        else:
+            print("SUCCESS!")
 
 def main():
     #Fill in List of Passwords
     passwords = []
-    entering = True
-    while entering:
-        #read passwords from file
-        addpassword(passwords, word)
+    play = True
+    while play:
+        enterPasswords(passwords)
+        correctKey = passwords[random.randint(0, len(passowrds) - 1)]
+        guessPasswordsGame(passwords, correctKey)
+        #loop is only running once for testing purposes
+        play = False
         
     
 
